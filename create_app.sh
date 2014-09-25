375f90d9a8d9e60168b800a9a28e83a65b7e8f06
sven <sven@so36.net>
2014-09-25 12:07:13 +0200

    OSX: Add first versrion of bundle build script - wip

---
+++ create_app.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# This will create a Apple Bundle App for installing Plone on OSX
+# Dependencies: osacompile
+
+# Declare some VARS
+APP_NAME="PloneOSX1.app"
+SOURCE_SCRIPT="install.plone.applescript"
+
+# Create the dir structure
+/usr/bin/osacompile -o "$APP_NAME" "$SOURCE_SCRIPT"
+
+# Copy applet.icns to the right place
+cp applet.icns "$APP_NAME"/Contents/Resources
+
+# Copy Plone icon, which we use for Displays
+cp plone.icns "$APP_NAME"/Contents/Resources
+
+# Copy scripts to Resources
+cp Scripts/* "$APP_NAME"/Contents/Resources/Scripts
